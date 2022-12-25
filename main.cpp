5fe9544ceb748ad6f47372495726278a78eb9b43
nicholasmaloney <nickmaster0712@gmail.com>
2022-12-24 22:32:12 -0800

    v0.04
    delta time implemented to velocity
    
    trying to get particles to interact with each other in a optimized way

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 #include <SFML/Graphics.hpp>
 #include "SFML/Window.hpp"
 #include <iostream>
-using namespace std;
+#include <ctime>
 #include "particle.hpp"
 #include "preformance.hpp"
 #include <stdio.h>
 #include<tuple>
 #include <vector>
 #include <random>
+#include <chrono>
+using namespace std;
 
 int worldsize[2] = {10, 6};
 int chunk_size = 100;
@@ -43,7 +45,7 @@ int main()
     
     sf::RenderWindow window(sf::VideoMode(1000, 600), "Space Particles");
     window.setVisible(true);
-    sf::CircleShape shape(10.0);
+    sf::CircleShape shape(1.0);
     shape.setFillColor(sf::Color::Green);
 
     sf::Vector2i position = sf::Mouse::getPosition(window);
@@ -53,18 +55,21 @@ int main()
     //auto pr;
 
     
-    
+    //vector<vector<vector<int> > > map = chunk();
     vector<particles> allp;
     cout << "hello" << endl;
     
+    
     for (int i = 0; i < 1000; i++)
     {
+        for (int a = 0; a < 50; a++){
         allp.push_back(particles());
-        allp[i].x = i;
-        allp[i].y = 2;
-        allp[i].damp = 1;
-        allp[i].id = 0;
-        
+        allp[allp.size() - 1].x = i;
+        allp[allp.size() - 1].y = a;
+        allp[allp.size() - 1].damp = 1;
+        allp[allp.size() - 1].id = 0;
+        //cout << allp[i].x << endl;
+        }
     }
 
     
@@ -72,9 +77,12 @@ int main()
     //cout << len(temp) << endl;
     shape.setPosition(allp[0].x, allp[0].y);
     vector<vector<vector<int> > > map;
-
+    auto start = std::chrono::system_clock::now();
+    auto end = std::chrono::system_clock::now();
+    double dt = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     while (window.isOpen())
-    {
+    {   
+        start = std::chrono::system_clock::now();
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -89,19 +97,22 @@ int main()
         {
         shape.setPosition(allp[i].x, allp[i].y);
         window.draw(shape);
-        //allp[i].addvelocity(position.x, position.y, -.0005);
-        allp[i].addvelocity(500, 300, -.0001);
+        allp[i].addvelocity(position.x, position.y, -.05);
+        //allp[i].addvelocity(500, 600, (-.0005 * dt));
         allp[i].update();
         }
         window.display();
-        map = chunk();
-
+        vector<vector<vector<int> > > map = chunk();
+        //cout << map[1][1][0] << endl;
         //out << map[1][1][0] << endl;
     
         position = sf::Mouse::getPosition(window);
         //window.setSize(sf::Vector2u(window.getSize()))
+    
         
-
+        end = std::chrono::system_clock::now();
+        dt = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        dt = dt;
 
     }
 
---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <chrono>
+#include <ctime>    
+
+int main()
+{
+    auto start = std::chrono::system_clock::now();
+    // Some computation here
+    auto end = std::chrono::system_clock::now();
+ 
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+ 
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds.count() << "s"
+              << std::endl;
+}
